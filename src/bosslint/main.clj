9f67f551b4d08cefa294c9878d065d9cd76fa4eb
Toshiki Takeuchi <roimisia@gmail.com>
2020-02-07 20:17:48 +0900

    Add help option

--- src/bosslint/main.clj
+++ src/bosslint/main.clj
@@ -1,7 +1,8 @@
 (ns bosslint.main
   (:require [clojure.java.io :as io]
             [clojure.java.shell :as shell]
-            [clojure.string :as string])
+            [clojure.string :as string]
+            [clojure.tools.cli :as cli])
   (:gen-class))
 
 (def excludes
@@ -106,10 +107,45 @@
     (eastwood files)
     (stylelint files)))
 
+(def cli-options
+  [["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["Usage: bosslint [<options>] [<commit>]"
+        ""
+        "Options:"
+        options-summary]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn validate-args [args]
+  (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
+    (cond
+      (:help options)
+      {:exit-message (usage summary) :ok? true}
+
+      errors
+      {:exit-message (error-msg errors)}
+
+      (<= (count arguments) 1)
+      {:ref (first arguments) :options options}
+
+      :else
+      {:exit-message (usage summary)})))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
 (defn -main [& args]
-  (try
-    (run (first args))
-    (System/exit 0)
-    (catch Exception e
-      (println (.getMessage e))
-      (System/exit 1))))
+  (let [{:keys [ref options exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (try
+        (run ref)
+        (System/exit 0)
+        (catch Exception e
+          (exit 1 (.getMessage e)))))))
