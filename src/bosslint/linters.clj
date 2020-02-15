61b2d3d2fa21dbf4595acf06396768ad8559e7a0
Toshiki Takeuchi <roimisia@gmail.com>
2020-02-15 14:27:59 +0900

    Support cljfmt and eastwood via clojure CLI

--- src/bosslint/linters.clj
+++ src/bosslint/linters.clj
@@ -1,5 +1,6 @@
 (ns bosslint.linters
   (:require [bosslint.util :as util]
+            [clojure.edn :as edn]
             [clojure.java.shell :as shell]
             [clojure.string :as string]))
 
@@ -33,19 +34,44 @@
                        (map :absolute-path files))]
         (println (:out ret))))))
 
-(defmethod lint ::cljfmt
+(defmethod lint :bosslint.linters.cljfmt/clojure
   [_ files]
   (let [files (select-files files [:clj :cljc :cljs])]
-    (when (and (util/command-exists? "lein")
-               (zero? (:exit (shell/sh "lein" "deps" ":tree")))
-               (seq files))
+    (when (seq files)
       (newline)
-      (println "cljfmt:")
+      (println "cljfmt (clojure):")
+      (print-files files)
+      (util/check-command "clojure")
+      (let [ret (apply shell/sh "clojure"
+                       "-Sdeps" "{:deps {cljfmt {:mvn/version \"0.6.4\"}}}"
+                       "-m" "cljfmt.main" "check"
+                       (map :absolute-path files))]
+        (println (:out ret))
+        (println (:err ret))))))
+
+(defmethod lint :bosslint.linters.cljfmt/lein
+  [_ files]
+  (let [files (select-files files [:clj :cljc :cljs])]
+    (when (and (seq files)
+               (util/command-exists? "lein")
+               (zero? (:exit (shell/sh "lein" "deps" ":tree")))
+               (not= (:out (shell/sh "lein" "cljfmt" "-h"))
+                     "Task: 'cljfmt' not found"))
+      (newline)
+      (println "cljfmt (lein):")
       (print-files files)
       (let [ret (apply shell/sh "lein" "cljfmt" "check"
                        (map :git-path files))]
         (println (:out ret))
         (println (:err ret))))))
+
+(defmethod lint ::cljfmt
+  [_ files]
+  (when (seq (select-files files [:clj :cljc :cljs]))
+    (when-let [key (cond
+                     (util/command-exists? "clojure") :bosslint.linters.cljfmt/clojure
+                     (util/command-exists? "lein") :bosslint.linters.cljfmt/lein)]
+      (lint key files))))
 
 (defmethod lint ::clj-kondo
   [_ files]
@@ -59,14 +85,31 @@
                        (map :absolute-path files))]
         (println (:out ret))))))
 
-(defmethod lint ::eastwood
+(defmethod lint :bosslint.linters.eastwood/clojure
   [_ files]
   (let [files (select-files files [:clj :cljc])]
-    (when (and (util/command-exists? "lein")
-               (zero? (:exit (shell/sh "lein" "deps" ":tree")))
-               (seq files))
+    (when (seq files)
       (newline)
-      (println "eastwood:")
+      (println "eastwood (clojure)")
+      (print-files files)
+      (let [ret (shell/sh "clojure"
+                          "-Sdeps" "{:deps {jonase/eastwood {:mvn/version \"RELEASE\"}}}"
+                          "-m" "eastwood.lint"
+                          (->> (map :git-path files)
+                               (map path->ns)
+                               (string/join " ")
+                               (format "{:namespaces [%s]}")))]
+        (println (:out ret))))))
+
+(defmethod lint :bosslint.linters.eastwood/lein
+  [_ files]
+  (let [files (select-files files [:clj :cljc])]
+    (when (and (seq files)
+               (util/command-exists? "lein")
+               (zero? (:exit (shell/sh "lein" "deps" ":tree")))
+               (zero? (:exit (shell/sh "lein" "eastwood" "help"))))
+      (newline)
+      (println "eastwood (lein):")
       (print-files files)
       (let [ret (shell/sh "lein" "eastwood"
                           (->> (map :git-path files)
@@ -74,6 +117,28 @@
                                (string/join " ")
                                (format "{:namespaces [%s]}")))]
         (println (:out ret))))))
+
+(defn- clojure-project? []
+  (let [{:keys [config-files]} (-> (shell/sh "clojure" "-Sdescribe")
+                                   :out
+                                   edn/read-string)]
+    (some? ((set config-files) "deps.edn"))))
+
+(defn- leiningen-project? []
+  (zero? (:exit (shell/sh "lein" "deps" ":tree"))))
+
+(defmethod lint ::eastwood
+  [_ files]
+  (when (seq (select-files files [:clj :cljc]))
+    (when-let [key (cond
+                     (and (util/command-exists? "clojure")
+                          (clojure-project?))
+                     :bosslint.linters.eastwood/clojure
+
+                     (and (util/command-exists? "lein")
+                          (leiningen-project?))
+                     :bosslint.linters.eastwood/lein)]
+      (lint key files))))
 
 (defmethod lint ::hadolint
   [_ files]
