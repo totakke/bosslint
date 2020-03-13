797f12de5c01d956688b08373e4c7b802401cf9f
Toshiki Takeuchi <roimisia@gmail.com>
2020-03-13 19:05:06 +0900

    Make version of cljfmt and eastwood configurable

--- src/bosslint/linter/cljfmt.clj
+++ src/bosslint/linter/cljfmt.clj
@@ -3,36 +3,37 @@
             [bosslint.linter :as linter :refer [deflinter]]
             [bosslint.util :as util]
             [clojure.java.shell :as shell]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [io.aviso.ansi :as ansi]))
 
 (defn- cljfmt-clojure
   [files conf]
-  (when (linter/check-command "clojure")
-    (let [opt-indents (config/resolve-path (:indents conf))
-          args (flatten
-                (cond-> ["clojure"
-                         "-Sdeps" "{:deps {cljfmt {:mvn/version \"0.6.4\"}}}"
-                         "-m" "cljfmt.main" "check"]
-                  opt-indents (concat ["--indents" opt-indents])
-                  true (concat (map :absolute-path files))))
-          ret (apply shell/sh args)]
-      (when-not (string/blank? (:out ret))
-        (println (string/trim-newline (:out ret))))
-      (when-not (string/blank? (:err ret))
-        (println (string/trim-newline (:err ret)))))))
+  (let [version (or (:version conf) "RELEASE")
+        opt-indents (config/resolve-path (:indents conf))
+        args (flatten
+              (cond-> ["clojure"
+                       "-Sdeps" (format "{:deps {cljfmt {:mvn/version \"%s\"}}}" version)
+                       "-m" "cljfmt.main" "check"]
+                opt-indents (concat ["--indents" opt-indents])
+                true (concat (map :absolute-path files))))
+        ret (apply shell/sh args)]
+    (when-not (string/blank? (:out ret))
+      (println (string/trim-newline (:out ret))))
+    (when-not (string/blank? (:err ret))
+      (println (string/trim-newline (:err ret))))))
 
 (defn- cljfmt-lein
-  [files _]
-  (when (and (util/command-exists? "lein")
-             (zero? (:exit (shell/sh "lein" "deps" ":tree")))
-             (not= (:out (shell/sh "lein" "cljfmt" "-h"))
-                   "Task: 'cljfmt' not found"))
-    (let [ret (apply shell/sh "lein" "cljfmt" "check"
-                     (map :git-path files))]
-      (when-not (string/blank? (:out ret))
-        (println (string/trim-newline (:out ret))))
-      (when-not (string/blank? (:err ret))
-        (println (string/trim-newline (:err ret)))))))
+  [files conf]
+  (let [version (or (:version conf) "RELEASE")
+        args (concat ["lein"
+                      "update-in" ":plugins" "conj" (format "[lein-cljfmt \"%s\"]" version)
+                      "--" "cljfmt" "check"]
+                     (map :git-path files))
+        ret (apply shell/sh args)]
+    (when-not (string/blank? (:out ret))
+      (println (string/trim-newline (:out ret))))
+    (when-not (string/blank? (:err ret))
+      (println (string/trim-newline (:err ret))))))
 
 (deflinter :linter/cljfmt
   (name [] "cljfmt")
@@ -41,11 +42,12 @@
     (linter/select-files file-group [:clj :cljc :cljs]))
 
   (lint [files conf]
-    (when-let [f (cond
-                   (and (linter/leiningen-project?)
-                        (util/command-exists? "lein"))
-                   cljfmt-lein
+    (if-let [f (cond
+                 (and (linter/leiningen-project?)
+                      (util/command-exists? "lein"))
+                 cljfmt-lein
 
-                   (util/command-exists? "clojure")
-                   cljfmt-clojure)]
-      (f files conf))))
+                 (util/command-exists? "clojure")
+                 cljfmt-clojure)]
+      (f files conf)
+      (println (ansi/yellow "Command not found: clojure or lein")))))
--- src/bosslint/linter/eastwood.clj
+++ src/bosslint/linter/eastwood.clj
@@ -2,7 +2,8 @@
   (:require [bosslint.linter :as linter :refer [deflinter]]
             [bosslint.util :as util]
             [clojure.java.shell :as shell]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [io.aviso.ansi :as ansi]))
 
 (def ^:private excludes
   [#"project.clj$"
@@ -15,9 +16,10 @@
       (string/replace #"/" ".")))
 
 (defn- eastwood-clojure
-  [files]
-  (let [ret (shell/sh "clojure"
-                      "-Sdeps" "{:deps {jonase/eastwood {:mvn/version \"RELEASE\"}}}"
+  [files conf]
+  (let [version (or (:version conf) "RELEASE")
+        ret (shell/sh "clojure"
+                      "-Sdeps" (format "{:deps {jonase/eastwood {:mvn/version \"%s\"}}}" version)
                       "-m" "eastwood.lint"
                       (->> (map :git-path files)
                            (map path->ns)
@@ -26,16 +28,17 @@
     (println (string/trim-newline (:out ret)))))
 
 (defn- eastwood-lein
-  [files]
-  (when (and (util/command-exists? "lein")
-             (zero? (:exit (shell/sh "lein" "deps" ":tree")))
-             (zero? (:exit (shell/sh "lein" "eastwood" "help"))))
-    (let [ret (shell/sh "lein" "eastwood"
-                        (->> (map :git-path files)
-                             (map path->ns)
-                             (string/join " ")
-                             (format "{:namespaces [%s]}")))]
-      (println (string/trim-newline (:out ret))))))
+  [files conf]
+  (let [version (or (:version conf) "RELEASE")
+        args ["lein"
+              "update-in" ":plugins" "conj" (format "[jonase/eastwood \"%s\"]" version)
+              "--" "eastwood"
+              (->> (map :git-path files)
+                   (map path->ns)
+                   (string/join " ")
+                   (format "{:namespaces [%s]}"))]
+        ret (apply shell/sh args)]
+    (println (string/trim-newline (:out ret)))))
 
 (deflinter :linter/eastwood
   (name [] "eastwood")
@@ -45,13 +48,14 @@
          (remove (fn [{:keys [git-path]}]
                    (some #(re-find % git-path) excludes)))))
 
-  (lint [files _]
-    (when-let [f (cond
-                   (and (util/command-exists? "clojure")
-                        (linter/clojure-project?))
-                   eastwood-clojure
+  (lint [files conf]
+    (if-let [f (cond
+                 (and (util/command-exists? "clojure")
+                      (linter/clojure-project?))
+                 eastwood-clojure
 
-                   (and (util/command-exists? "lein")
-                        (linter/leiningen-project?))
-                   eastwood-lein)]
-      (f files))))
+                 (and (util/command-exists? "lein")
+                      (linter/leiningen-project?))
+                 eastwood-lein)]
+      (f files conf)
+      (println (ansi/yellow "Command not found: clojure or lein")))))
