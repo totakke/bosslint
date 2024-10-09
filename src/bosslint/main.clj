0c81136f15cb99e269aa6592d7bd03b732098d09
Toshiki Takeuchi <roimisia@gmail.com>
2024-10-09 21:24:40 +0900

    Prepare for next development iteration (0.5.0-SNAPSHOT)

--- src/bosslint/main.clj
+++ src/bosslint/main.clj
@@ -12,7 +12,7 @@
             [io.aviso.ansi :as ansi])
   (:gen-class))
 
-(def version "0.4.1")
+(def version "0.5.0-SNAPSHOT")
 
 (defn- list-linters []
   (sort-by linter/name (descendants :bosslint/linter)))
