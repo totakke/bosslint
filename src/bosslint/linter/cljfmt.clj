93834f664f5cbd62a3806c9588445d0951f8f24e
Toshiki Takeuchi <roimisia@gmail.com>
2025-02-24 15:55:25 +0900

    cljfmt: support standalone cli

--- src/bosslint/linter/cljfmt.clj
+++ src/bosslint/linter/cljfmt.clj
@@ -5,6 +5,16 @@
 
 (def ^:private cljfmt-artifact "dev.weavejester/cljfmt")
 
+(defn- cljfmt-cli
+  [files conf]
+  (let [args (concat ["cljfmt" "check"]
+                     (:command-options conf)
+                     (map :absolute-path files))]
+    (case (apply process/run args)
+      0 :success
+      1 :warning
+      :error)))
+
 (defn- cljfmt-clojure
   [files conf]
   (let [version (or (:version conf) "RELEASE")
@@ -12,7 +22,8 @@
                       "-Sdeps" (format "{:deps {%s {:mvn/version \"%s\"}}}"
                                        cljfmt-artifact version)
                       "-M" "-m" "cljfmt.main" "check"]
-                     (:command-options (:clojure conf))
+                     (or (:command-options conf)
+                         (:command-options (:clojure conf)))
                      (map :absolute-path files))]
     (case (apply process/run args)
       0 :success
@@ -40,6 +51,9 @@
 
   (lint [files conf]
     (if-let [f (cond
+                 (process/command-exists? "cljfmt")
+                 cljfmt-cli
+
                  (and (linter/leiningen-project?)
                       (process/command-exists? "lein"))
                  cljfmt-lein
